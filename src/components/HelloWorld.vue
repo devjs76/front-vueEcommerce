635d537927f89d53954469861aa86c739f2ed5b2
devjs76 <byrd4299@gmail.com>
2019-06-26 23:52:49 -0500

    changes card

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,32 @@
 <template>
   <v-container>
-     <v-layout row>
-        <v-flex xs12 sm3 pa-3 v-for='element in elements' :key='element.id' :element='element'>
+     <v-layout row wrap>
+        <v-flex xs12 md3 pa-3 v-for='(element,index) in elements' :key='element.id' :element='element'>
+         
           <v-card>
             <v-img v-bind:src=element.imagen height="200px"></v-img>
             <v-card-title primary-title>
               <div>
-                <div class="headline">{{element.nombrePizza}}</div>
-                <span class="green--text">{{element.precioBase}}</span>
+                <div class="headline">{{element.nombre}}</div>
+                <span class="green--text">${{element.precio}}  {{array[index]}} </span>
               </div>
+              <v-btn flat color="blue" :to="{name:'order',params: {id: element.id}}">Ordenar</v-btn>
             </v-card-title>
 
             <v-card-actions>
-              <v-btn flat color="blue" :to="{name:'order',params: {id: element.id}}">Ordenar</v-btn>
-              <v-spacer></v-spacer>
-              <v-btn icon @click="show = !show">
-                <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-              </v-btn>
+            <v-expansion-panel>
+    <v-expansion-panel-content
+      :key="index">
+      <template v-slot:header>
+        <span flat color="blue">Descripcion</span>
+        <div></div>
+      </template>
+      <v-card>
+        <v-card-text>{{element.descripcion}}</v-card-text>
+      </v-card>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
             </v-card-actions>
-            <v-slide-y-transition>
-              <v-card-text v-show="show">
-                I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-              </v-card-text>
-            </v-slide-y-transition>
          </v-card>
         </v-flex>
     </v-layout>
@@ -30,10 +34,13 @@
 </template>
 
 <script>
+  
   export default {
-    data: () => ({
+    data (){
+      return {
       show: false,
       elements: [],
+      array:[],
       element: {
         id: null,
         nombrePizza: null,
@@ -41,7 +48,10 @@
         imagen: null,
         precioBase: null
       }
-    }),
+      }
+
+      
+    },
     created () {
       this.getElements()
     },
@@ -51,11 +61,22 @@
         .then(response => {
           console.log(response.data)
           this.elements = response.data
+          for (let index = 0; index < this.elements.length; index++) {
+            this.array.push(false)
+          }
+          console.log(this.array)
         })
         .catch( e => {
           console.log(e)
         })
-      }
+      },
+
+    mostrar(index){
+     array[index]=!array[index];
+      //console.log("dentro: ",this.array[index], " index:",index)
+     // return this.array
+      console.log(array)
+    }
     }
   }
 </script>
