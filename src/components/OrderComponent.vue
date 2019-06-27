0ea529e4984ec81edc3dd02489527ed3bbfec1df
devjs76 <byrd4299@gmail.com>
2019-06-27 02:13:46 -0500

    styles

--- src/components/OrderComponent.vue
+++ src/components/OrderComponent.vue
@@ -1,69 +1,76 @@
 <template>
-    <div>
-        <v-stepper v-model="e1">
-            <v-stepper-header>
-            <v-stepper-step :complete="e1 > 1" step="1">Elige tu pizza</v-stepper-step>
-            <v-divider></v-divider>
-            <v-stepper-step :complete="e1 > 2" step="2">Llena tus datos</v-stepper-step>
-            <v-divider></v-divider>
-            <v-stepper-step step="3">Confirma tu pedido</v-stepper-step>
-            </v-stepper-header>
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12 mb12 sm12 >
+          <v-stepper v-model="e1">
+              <v-stepper-header>
+              <v-stepper-step :complete="e1 > 1" step="1">Elige tu pizza</v-stepper-step>
+              <v-divider></v-divider>
+              <v-stepper-step :complete="e1 > 2" step="2">Llena tus datos</v-stepper-step>
+              <v-divider></v-divider>
+              <v-stepper-step step="3">Confirma tu pedido</v-stepper-step>
+              </v-stepper-header>
 
-            <v-stepper-items>
-                <v-stepper-content step="1">
-                    <v-form ref="form" v-model="valid" lazy-validation>
-                        <v-chip color="orange" text-color="white">{{element.nombrePizza}}<v-icon right>fas fa-utensils</v-icon></v-chip>
+              <v-stepper-items>
+                  <v-stepper-content step="1">
+                      <v-form ref="form" v-model="valid" lazy-validation>
+                          <v-chip color="orange" text-color="white">{{element.nombrePizza}}<!-- <v-icon right>fas fa-utensils</v-icon> --></v-chip>
+                          <v-flex mb3 xs3 sm3>
+                          <v-select v-model="select" :items="items" :rules="[v => !!v || 'Item is required']" label="Seleccione el tamaño de la pizza" required></v-select>
+                          </v-flex>
+                          <v-chip label color="pink" text-color="white" v-if="select=='Chica'">Precio: ${{element.precioBase+70}}<v-icon right>fas fa-dollar-sign</v-icon></v-chip>
+                          <v-chip label color="pink" text-color="white" v-if="select=='Mediana'">Precio: ${{element.precioBase+100}}</v-chip>
+                          <v-chip label color="pink" text-color="white" v-if="select=='Grande'">Precio: ${{element.precioBase+140}}</v-chip>
+                          <br>
+                          <v-btn color="primary" @click="e1 = 2"  :disabled="!valid">Continue</v-btn>
+                          <v-btn flat color="error" to="/">Cancelar</v-btn>
+                      </v-form> 
+                  </v-stepper-content>
+              
+                  <v-stepper-content step="2">
+                    <v-flex mb6 xs6 sm6>
+                      <v-form ref="form" v-model="valid" lazy-validation>
+                          <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Nombre" required></v-text-field>      
+                          <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
+                          <v-text-field v-model="number"  label="Número de teléfono" required></v-text-field>
+                          <v-text-field v-model="direccion" label="Dirección" required></v-text-field>
+                          <v-text-field v-model="referencias"  label="Referencias de la casa" required></v-text-field>                           
+                          <v-btn color="primary" @click="e1 = 3">Continue</v-btn>
+                          <v-btn flat color="error" to="/">Cancelar</v-btn>
+                      </v-form>
+                    </v-flex>
+                  </v-stepper-content>
 
-                        <v-select v-model="select" :items="items" :rules="[v => !!v || 'Item is required']" label="Seleccione el tamaño de la pizza" required></v-select>
-                    
-                         <v-chip label color="pink" text-color="white" v-if="select=='Chica'">Precio: ${{element.precioBase+70}}<v-icon right>fas fa-dollar-sign</v-icon></v-chip>
-                         <v-chip label color="pink" text-color="white" v-if="select=='Mediana'">Precio: ${{element.precioBase+100}}</v-chip>
-                         <v-chip label color="pink" text-color="white" v-if="select=='Grande'">Precio: ${{element.precioBase+140}}</v-chip>
-                         <br>
-                        <v-btn color="primary" @click="e1 = 2"  :disabled="!valid">Continue</v-btn>
-                        <v-btn flat color="error" to="/">Cancelar</v-btn>
-                    </v-form> 
-                </v-stepper-content>
-            
-                <v-stepper-content step="2">
-                    <v-form ref="form" v-model="valid" lazy-validation>
-                        <v-text-field v-model="name" :counter="10" :rules="nameRules" label="Nombre" required></v-text-field>      
-                        <v-text-field v-model="email" :rules="emailRules" label="E-mail" required></v-text-field>
-                        <v-text-field v-model="number"  label="Número de teléfono" required></v-text-field>
-                        <v-text-field v-model="direccion" label="Dirección" required></v-text-field>
-                        <v-text-field v-model="referencias"  label="Referencias de la casa" required></v-text-field>                           
-                        <v-btn color="primary" @click="e1 = 3">Continue</v-btn>
-                        <v-btn flat color="error" to="/">Cancelar</v-btn>
-                    </v-form>
-                </v-stepper-content>
+                  <v-stepper-content step="3">
+                      <v-card >
+                          <v-flex mb12 xs12 sm12> <v-img v-bind:src=element.imagen height="350px"></v-img> </v-flex>
+                          <v-card-title primary-title>
+                          <div>
+                              <h3 class="headline mb-0">Resumen de pedido</h3>
+                              <div> 
+                                  <h4>{{element.nombrePizza}}</h4>
+                                  <p>Total a pagar: </p>
+                                    <v-chip label color="blue"  text-color="white" v-if="select=='Chica'">{{element.precioBase+70}}</v-chip>
+                                    <v-chip label color="blue" text-color="white" v-if="select=='Mediana'">{{element.precioBase+100}}</v-chip>
+                                    <v-chip label color="blue" text-color="white" v-if="select=='Grande'">{{element.precioBase+140}}</v-chip>
 
-                <v-stepper-content step="3">
-                    <v-card>
-                        <v-img v-bind:src=element.imagen height="200px"></v-img>
-                        <v-card-title primary-title>
-                        <div>
-                            <h3 class="headline mb-0">Resumen de pedido</h3>
-                            <div> 
-                                <h4>{{element.nombrePizza}}</h4>
-                                <p>Total a pagar: </p>
-                                  <v-chip label color="blue"  text-color="white" v-if="select=='Chica'">{{element.precioBase+70}}</v-chip>
-                                  <v-chip label color="blue" text-color="white" v-if="select=='Mediana'">{{element.precioBase+100}}</v-chip>
-                                  <v-chip label color="blue" text-color="white" v-if="select=='Grande'">{{element.precioBase+140}}</v-chip>
-
-                                <p>Dirección: {{this.direccion}} </p>
-                                <p>Nombre de cliente: Jesús Israel Santiago Gutiérrez</p>
-                                
-                            </div>
-                        </div>
-                        </v-card-title>
-                                  <div ref="card"></div>
-                    </v-card>
-                    <v-btn color="primary" @click="purchase()">Pagar</v-btn>
-                    <v-btn flat color="error" to="/">Cancelar</v-btn>
-                </v-stepper-content>
-            </v-stepper-items>
-        </v-stepper>
-    </div>
+                                  <p>Dirección: {{this.direccion}} </p>
+                                  <p>Nombre de cliente: {{name}}</p>
+                                  <br>
+                                  <v-flex mb12 xs12 sm12><div ref="card" style="height:25px"></div></v-flex>        
+                              </div>
+                          </div>
+                          </v-card-title>
+                          
+                      </v-card>
+                      <v-btn color="primary" @click="purchase()">Pagar</v-btn>
+                      <v-btn flat color="error" to="/">Cancelar</v-btn>
+                  </v-stepper-content>
+              </v-stepper-items>
+          </v-stepper>
+        </v-flex>
+      </v-layout> 
+    </v-container>
 </template>
 
 <script >
